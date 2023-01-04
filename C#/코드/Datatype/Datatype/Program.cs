bb65e6fd09eefde8b560d1534825b153d1f21f2c
SEONGGEON KIM <rookims1996@hotmail.com>
2023-01-04 22:17:43 +0900

    WPF, C# 내용 추가
    WPF 이벤트 라우팅 시스템, C# 기초 자료형, 값을 다루는 기본 개념

---
+++ C#/코드/Datatype/Datatype/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace MyDatatype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // -------------------------------------------------
+            int n1 = 50;        // 선언과 동시에 초기화
+            int n2;
+            n2 = 100;           // 선언 후 초기화
+
+            int sum = n1 + n2;
+            Console.WriteLine(sum);
+
+            Console.WriteLine();
+            // -------------------------------------------------
+            float f = 5.2f;                     // float, decimal 기본 타입은 사용시 실수 뒤에 f, m을 각각 붙여야함
+            double d = 10.5;
+            decimal money = 200.099m;
+
+            Console.WriteLine();
+            // -------------------------------------------------
+            char ch1 = 'A';
+            char ch2 = 'B';
+            // char charSum1 = ch1 + ch2;        // ushort와 같은 범위의 수를 나타낼 수 있는 char이지만 사칙연산 후 다시 char에 넣는 것은 불가
+            
+            Console.WriteLine(ch1 + ch2);
+            Console.WriteLine(ch1 + " " + ch2);
+
+            Console.WriteLine();
+            // -------------------------------------------------
+            char es1 = '\n';                    // escape sequence를 활용해서 키보드로 입력이 불가능한 문자를 표현할 수 있음
+            char es2 = '\t';
+            char es3 = '\'';
+            char es4 = '\\';                    // 막상 출력이 불가능한 \를 출력할때 사용
+            char es5 = '\u2023';                // Unicode를 출력할때 사용 es4는 '▶'와 대응
+            Console.Write(ch1);
+            Console.Write(es1);
+            Console.Write(ch2);
+            Console.Write(es2);
+            Console.Write(ch2);
+            Console.Write(es3);
+            Console.Write(es4);
+            Console.WriteLine(es5);             // cmd에는 폰트가 존재하지 않아 ?로 출력
+
+            Console.WriteLine();
+            // -------------------------------------------------
+            string text1 = "Hello, World";
+            Console.WriteLine(text1);
+            text1 = "\"Hello, World\"\n";		// 문자열에 대한 escape sequence 활용
+            Console.Write(text1);
+            text1 = @"\tHello, World\n";		// @을 이용해 escape sequence 무시 가능
+            Console.WriteLine(text1);
+
+            text1 = "Concatenate ";
+            string text2 = "String";
+            string text3 = text1 + text2;		// 문자열은 +연산을 활용해서 붙이기 가능
+            Console.WriteLine(text3);
+
+            Console.WriteLine();
+            // -------------------------------------------------
+            Console.WriteLine(typeof(int));             // int와 같은 기본 타입은 닷넷 형식에 대한 C#의 예약어
+            Console.WriteLine(typeof(System.Int32));
+
+            Console.WriteLine();
+            // -------------------------------------------------
+        }
+    }
+}
---
+++ C#/코드/HandleValue/HandleValue/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace HandleValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // -------------------------------------------------
+            byte b = 250;   // 1바이트
+            short s = b;    // 1바이트 --> 2바이트의 암시적 변환
+            Console.Write(b);
+            Console.Write(' ');
+            Console.WriteLine(s);
+
+            ushort u = 65;  // 2바이트
+            char c = 'a';   // 2바이트, UTF16의 97번 문자
+            //char c = u;     // 2바이트 --> 2바이트이지만 char는 문자만 저장해야하므로 암시적 변환 실패
+            c = (char)u;    // 명시적 변환으로 강제로 변환은 가능
+            Console.Write(u);
+            Console.Write(' ');
+            Console.WriteLine(c);
+            u = (ushort)c;
+
+            int n = 40000;
+            s = (short)n;
+            Console.Write(n);
+            Console.Write(' ');
+            Console.WriteLine(s);
+            // -------------------------------------------------
+            const bool result = false;
+            //result = true;        // 상수는 변경할 수 없음
+            int maxN = Math.Max(0, 5);          // 결과가 5임을 확실히 알 수 있고 이는 변수에 대입 가능
+            //const int constMaxN = Math.Max(0, 5);    // 결과가 5임이 당연하지만 컴파일러 입장에서는 판단이 불가능하므로 불가능
+            const int max = 100 + 100;              // 수식으로 돼있는 경우에는 컴파일러가 바로 계산 가능하므로 상수 가능
+            // -------------------------------------------------
+        }
+    }
+}
