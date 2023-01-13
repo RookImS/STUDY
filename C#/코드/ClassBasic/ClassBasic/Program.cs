d617b158489229bc8484d5d1be337911c798698d
SEONGGEON KIM <rookims1996@hotmail.com>
2023-01-13 22:11:54 +0900

    C# 내용추가
    네임스페이스, 캡슐화, 프로퍼티 내용 추가

--- C#/코드/ClassBasic/ClassBasic/Program.cs
+++ C#/코드/ClassBasic/ClassBasic/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace ClassBasic
+﻿using System;
+using System.Numerics;
+
+namespace ClassBasic
 {
     class CellPhone
     {
@@ -41,6 +44,25 @@
         public int a;
         public int b;
         private int sum;
+        private double pi;
+        private double radius;
+
+        // 프로퍼티
+        public double Pi
+        {
+            get { return pi; }
+        }
+
+        public double Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+        public static int NumCalcInClass
+        {
+            get { return numCalcInClass; }
+        }
 
         // 여러 종류의 생성자
         public Mathematics()                // 기본 생성자와 닮은 생성자
@@ -48,6 +70,7 @@
             a = 0;
             b = 0;
             sum = 0;
+            pi = 3.14;
         }
 
         public Mathematics(int na, int nb)  // 2개의 매개변수를 받는 생성자
@@ -55,6 +78,7 @@
             a = na;
             b = nb;
             sum = 0;
+            pi = 3.14;
         }
 
         static Mathematics()                // 정적 생성자
@@ -65,6 +89,7 @@
 
         public static int GetCalcNum()             // 현재까지의 계산 수를 얻기 위해 사용하는 public 정적 메서드
         {
+            numUsageOfClass++;
             return numCalcInClass;
         }
 
@@ -130,7 +155,6 @@
         }
     }
 
-
     class CtorTest1
     {
     }
@@ -170,8 +194,8 @@
             Mathematics math1 = new Mathematics(1, 2);      // 매개변수 존재하는 인스턴스 생성자로 실행, Mathematics 클래스가 처음 사용됐으므로 정적 생성자 자동으로 사용
             Mathematics math2 = new Mathematics();          // 매개변수 존재하지 않는 인스턴스 생성자로 실행
 
-            math1.a = 5;                // 인스턴스 필드는 인스턴스를 통해 접근 가능
-            math1.b = 3;
+            math2.a = 5;                // 인스턴스 필드는 인스턴스를 통해 접근 가능
+            math2.b = 0;
             //math.sum = 4;             // 인스턴스 필드이지만 private이므로 접근 불가능
 
             //int sum = math1.plus();                               // 인스턴스 메서드를 사용했지만 private이라 접근 불가능
@@ -179,12 +203,20 @@
             Console.WriteLine("계산 결과 : " + sum);
 
             sum = math2.GetSum('/');                                // 인스턴스 메서드를 사용, 정적 필드 numUsageOfClass 1 증가, b가 0일 때는 나누기가 안되므로 numCalcInClass는 증가 안함
-
+            
             //Console.WriteLine("클래스 사용된 횟수 : " + math1.numUsageOfClass);           // 정적 필드는 인스턴스를 통해 접근 불가
             Console.WriteLine("클래스 사용된 횟수 : " + Mathematics.numUsageOfClass);       // 정적 필드는 클래스를 통해 접근
             //Console.WriteLine("클래스 내 계산의 횟수 : " + Mathematics.numCalcInClass);   // 정적 필드를 정상적으로 접근했지만 private이라 접근 불가능 
             Console.WriteLine("클래스 내 계산의 횟수 : " + Mathematics.GetCalcNum());       // 정적 메서드를 통해서 간접적으로 private값에 접근
             Console.WriteLine("이상한 계산 결과 : " + math1.GetStrangeSum());               // 내부에서 정적 필드를 사용한 인스턴스 메서드
+
+            Console.WriteLine();
+            // -------------------------------------------------
+            //math1.radius = 10;    // private 필드는 사용 불가능
+            math1.Radius = 10;      // private 필드에 대한 프로퍼티 내에 set이 있으므로 사용 가능
+            //math1.Pi = 5;         // 프로퍼티 내에 get밖에 없으므로 사용 불가능
+            Console.WriteLine("프로퍼티의 사용 : " + math1.Pi + ' ' + math1.Radius);
+            Console.WriteLine("클래스 내 계산의 횟수 : " + Mathematics.NumCalcInClass);      // 정적 필드 또한 프로퍼티 사용 가능
             // -------------------------------------------------
         }
     }
